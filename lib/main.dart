1cafdaa04b9061f66a817f84070907550f0ed9e5
Bakir Alkaraki <181054@ppu.edu.ps>
2023-01-01 13:44:54 +0200

    Create Type Adapter for noteModel

--- lib/main.dart
+++ lib/main.dart
@@ -3,9 +3,12 @@ import 'package:hive_flutter/hive_flutter.dart';
 import 'package:noteshive/constants.dart';
 import 'package:noteshive/view/notes_view.dart';
 
+import 'model/note_model.dart';
+
 void main() async {
   await Hive.initFlutter();
   await Hive.openBox(kNoteBox);
+  Hive.registerAdapter(NoteModelAdapter());
   runApp(const NotesApp());
 }
 
