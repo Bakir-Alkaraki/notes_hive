691a816ac825b912da3dbf870611a5a66c1abe64
Bakir Alkaraki <181054@ppu.edu.ps>
2023-01-02 15:20:07 +0200

    Create List for Notes

--- lib/cubits/cubit/notes_cubit.dart
+++ lib/cubits/cubit/notes_cubit.dart
@@ -2,8 +2,21 @@ import 'package:bloc/bloc.dart';
 import 'package:meta/meta.dart';
 import 'package:noteshive/model/note_model.dart';
 
+import 'package:hive_flutter/adapters.dart';
+import 'package:noteshive/constants.dart';
+
 part 'notes_state.dart';
 
 class NotesCubit extends Cubit<NotesState> {
   NotesCubit() : super(NotesInitial());
+
+  fetchAllNotes() {
+    try {
+      Box<NoteModel> notesBox = Hive.box<NoteModel>(kNoteBox);
+      List<NoteModel> notes = notesBox.values.toList();
+      emit(NotesSuccess(notes));
+    } catch (e) {
+      emit(NotesFailure(e.toString()));
+    }
+  }
 }
