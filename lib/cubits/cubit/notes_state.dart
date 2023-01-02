376cc9660017ceda361993625fae91d3164d5691
Bakir Alkaraki <181054@ppu.edu.ps>
2023-01-02 15:12:21 +0200

    Fix keyboard overflow the screen

---
+++ lib/cubits/cubit/notes_state.dart
@@ -0,0 +1,20 @@
+part of 'notes_cubit.dart';
+
+@immutable
+abstract class NotesState {}
+
+class NotesInitial extends NotesState {}
+
+class NotesLoading extends NotesState {}
+
+class NotesSuccess extends NotesState {
+  final List<NoteModel> list;
+
+  NotesSuccess(this.list);
+}
+
+class NotesFailure extends NotesState {
+  final String errMsg;
+
+  NotesFailure(this.errMsg);
+}
