3354b05590f1c147eddd62243a7e049c5a03751f
Bakir Alkaraki <181054@ppu.edu.ps>
2023-01-04 12:36:45 +0200

    Create Colors Picker part 2

--- lib/view/widgets/colors_list_picker.dart
+++ lib/view/widgets/colors_list_picker.dart
@@ -1,19 +1,47 @@
 import 'package:flutter/material.dart';
 
-class ColorCircle extends StatelessWidget {
-  const ColorCircle({Key? key}) : super(key: key);
+class ColorItem extends StatelessWidget {
+  const ColorItem({Key? key, required this.isPicked, required this.color})
+      : super(key: key);
+
+  final bool isPicked;
+  final Color color;
 
   @override
   Widget build(BuildContext context) {
-    return const CircleAvatar(
-      radius: 38,
-      backgroundColor: Colors.blue,
-    );
+    return isPicked
+        ? CircleAvatar(
+            radius: 38,
+            backgroundColor: Colors.white,
+            child: CircleAvatar(
+              radius: 36,
+              backgroundColor: color,
+            ),
+          )
+        : CircleAvatar(
+            radius: 38,
+            backgroundColor: color,
+          );
   }
 }
 
-class ColorsList extends StatelessWidget {
+class ColorsList extends StatefulWidget {
   const ColorsList({Key? key}) : super(key: key);
+
+  @override
+  State<ColorsList> createState() => _ColorsListState();
+}
+
+class _ColorsListState extends State<ColorsList> {
+  int currentIndex = 0;
+
+  List<Color> colors = const [
+    Color(0xffBAD1CD),
+    Color(0xffF2D1C9),
+    Color(0xffE086D3),
+    Color(0xff8332AC),
+    Color(0xff462749),
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -21,9 +49,20 @@ class ColorsList extends StatelessWidget {
         height: 38 * 2,
         child: ListView.builder(
             scrollDirection: Axis.horizontal,
-            itemCount: 10,
+            itemCount: colors.length,
             itemBuilder: (context, index) {
-              return const ColorCircle();
+              return Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 6),
+                child: GestureDetector(
+                    onTap: () {
+                      currentIndex = index;
+                      setState(() {});
+                    },
+                    child: ColorItem(
+                      isPicked: currentIndex == index,
+                      color: colors[index],
+                    )),
+              );
             }));
   }
 }
